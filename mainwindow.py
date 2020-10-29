7da2fea45a8e7f2e54e7cb1240d670c895ec2d54
Ramón García <ramon.garcia5800.alumnos.udg.mx>
2020-10-29 13:56:15 -0600

    Actividad 11 guardar y abrir documentos json

---
+++ mainwindow.py
@@ -0,0 +1,105 @@
+from PySide2.QtWidgets import QMainWindow, QFileDialog, QMessageBox
+from PySide2.QtCore import Slot
+from ui_mainwindow import Ui_MainWindow
+from particula.particulas import Particulas
+from particula.particula import Particula
+from particula.algoritmos import distancia_euclidiana
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+
+        self.particulas = Particulas()
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.ui.agregar_final_pushButton.clicked.connect(self.click_agregar_final)
+        self.ui.agregar_inicio_pushButton.clicked.connect(self.click_agregar_inicio)
+        self.ui.mostrar_pushButton.clicked.connect(self.click_mostrar)
+
+        self.ui.actionAbrir.triggered.connect(self.action_abrir_archivo)
+        self.ui.actionGuardar.triggered.connect(self.action_guardar_archivo)
+
+    @Slot()
+    def action_abrir_archivo(self):
+        # print('abrir_archivo')
+        ubicacion = QFileDialog.getOpenFileName(
+            self,
+            'Abrir Archivo',
+            '.',
+            'JSON (*.json)'
+        )[0]
+        if self.particulas.abrir(ubicacion):
+            QMessageBox.information(
+                self,
+                "Exito",
+                "Se abrio el archivo " + ubicacion
+            )
+        else:
+            QMessageBox.critical(
+                self,
+                "Error",
+                "Error al abrir el archivo " + ubicacion
+            )
+
+    @Slot()
+    def action_guardar_archivo(self):
+        # print('guardar_archivo')
+        ubicacion = QFileDialog.getSaveFileName(
+            self,
+            'Guardar Archivo',
+            '.',
+            'JSON (*.json)'
+        )[0]
+        print(ubicacion)
+        if self.particulas.guardar(ubicacion):
+            QMessageBox.information(
+                self,
+                "Exito",
+                "Se pudo crear el archivo " + ubicacion
+            )
+        else:
+            QMessageBox.critical(
+                self,
+                "Error",
+                "No se pudo crear el archivo " + ubicacion
+            )
+
+    @Slot()
+    def click_mostrar(self):
+        # self.particulas.mostrar()
+        self.ui.salida.clear()
+        self.ui.salida.insertPlainText(str(self.particulas))
+
+
+    @Slot()
+    def click_agregar_final(self):
+        id = self.ui.id_spinBox.value()
+        origen_x = self.ui.origen_x_spinBox.value()
+        origen_y = self.ui.origne_y_spinBox.value()
+        destino_x = self.ui.destino_x_spinBox.value()
+        destino_y = self.ui.destino_y_spinBox.value()
+        velocidad = self.ui.velocidad_spinBox.value()
+        red = self.ui.red_spinBox.value()
+        green = self.ui.green_spinBox.value()
+        blue = self.ui.blue_spinBox.value()
+
+        particula = Particula(id, origen_x, origen_y, destino_x, destino_y, velocidad, red, green, blue, distancia = distancia_euclidiana)
+        self.particulas.agregar_final(particula)
+
+        # print(id, origen_x, origen_y, destino_x, destino_y, velocidad, red, green, blue)
+        # self.ui.salida.insertPlainText(str(id) + str(origen_x) + str(origen_y) + str(destino_x) + str(destino_y) + str(velocidad) + str(red) + str(green) + str(blue) )
+    @Slot()
+    def click_agregar_inicio(self):
+        id = self.ui.id_spinBox.value()
+        origen_x = self.ui.origen_x_spinBox.value()
+        origen_y = self.ui.origne_y_spinBox.value()
+        destino_x = self.ui.destino_x_spinBox.value()
+        destino_y = self.ui.destino_y_spinBox.value()
+        velocidad = self.ui.velocidad_spinBox.value()
+        red = self.ui.red_spinBox.value()
+        green = self.ui.green_spinBox.value()
+        blue = self.ui.blue_spinBox.value()
+
+        particula = Particula(id, origen_x, origen_y, destino_x, destino_y, velocidad, red, green, blue, distancia = distancia_euclidiana)
+        self.particulas.agregar_inicio(particula)
